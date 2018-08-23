8dd54efa52b6ef504a7e01e606e31aa2dffa18a6
Siarhei Plistniou <siarhei_pliatniou@epam.com>
2018-08-23 04:33:40 +0300

    fix script

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function generateReport(options) {
     return reporter.generate(options);
 }
 
-const pathToReport = (yargs.path) ? yargs.path : './reports/report_epam.json' ;
+const pathToReport = yargs.path;
 let options = {
     jsonFile: pathToReport,
     output: './reports/custom_cucumber_report.html'
